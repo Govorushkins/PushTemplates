cfdc4c999e20b3d4830729acb0d1c115aac69538
antongovorushkin <govorushkinanton@gmail.com>
2018-09-21 12:07:39 +0300

    Refactored in order to enable GCM messaging support

--- app/src/main/java/com/testing/backendless_messaging_test/activities/SampleSQLiteDBHelper.java
+++ app/src/main/java/com/testing/backendless_messaging_test/activities/SampleSQLiteDBHelper.java
@@ -5,10 +5,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by oleg on 1/26/18.
- */
-
 public class SampleSQLiteDBHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "messaging";
